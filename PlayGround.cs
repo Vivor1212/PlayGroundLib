fd923255dcd938acbbee5dbd1e73802f6844782f
Vivor1212 <syhlerp@yahoo.dk>
2025-11-06 10:39:18 +0100

    Add project files.

---
+++ PlayGround.cs
@@ -0,0 +1,41 @@
+﻿namespace PlayGroundLib
+{
+    public class PlayGround
+    {
+        private int id;
+        private string name;
+        private int maxChildren;
+        private int minAge;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Name
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+
+        public int MaxChildren
+        {
+            get { return MaxChildren; }
+            set { MaxChildren = value; }
+        }
+
+        public int MinAge
+        {
+            get { return MinAge; }
+            set { MinAge = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"PlayGround [Id={id}, Name={name}, MaxChildren={maxChildren}, MinAge={minAge}]";
+        }
+
+
+    }
+}
---
+++ PlayGroundRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayGroundLib
+{
+    public class PlayGroundRepository
+    {
+
+        private int _nextId = 1;
+
+        private List<PlayGround> playGrounds = new List<PlayGround>()
+        {
+            new PlayGround { Id = 1, Name = "Millpark", MaxChildren = 10, MinAge = 5 },
+            new PlayGround { Id = 2, Name = "Secret Playground", MaxChildren = 12, MinAge = 4 },
+            new PlayGround { Id = 3, Name = "Library", MaxChildren = 8, MinAge = 3 },
+            new PlayGround { Id = 4, Name = "School", MaxChildren = 15, MinAge = 7 }
+        };
+
+        public List<PlayGround> GetAll()
+        {
+            return playGrounds;
+        }
+
+
+        public PlayGround GetById(int id)
+        {
+            return playGrounds.FirstOrDefault(pg => pg.Id == id);
+        }
+        public void AddP(PlayGround playGr)
+        {
+            playGr.Id = _nextId++;
+            playGrounds.Add(playGr);
+        }
+
+        public void UpdateP(PlayGround playGr)
+        {
+            var existingPlayGr = GetById(playGr.Id);
+            if (existingPlayGr != null)
+            {
+                existingPlayGr.Name = playGr.Name;
+                existingPlayGr.MaxChildren = playGr.MaxChildren;
+                existingPlayGr.MinAge = playGr.MinAge;
+            }
+        }
+    }
+}
